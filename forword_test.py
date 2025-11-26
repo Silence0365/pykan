f7d60679b013488ac89ff69d5ee10b1be87a08f7
Silence0365 <1161378802@qq.com>
2025-11-27 01:59:06 +0800

    前向传播学习

--- forword_test.py
+++ forword_test.py
@@ -1,8 +1,14 @@
 from kan import *
+import torch
 model = KAN(width=[2,5,1], grid=5, k=3, seed=0)# 模型创建
-x = torch.rand(100,2) # 生成传递数据
+#model = KAN.loadckpt('./model/0.10')
+# x = torch.tensor([[0.15,0.2235],
+#                  [0.323,0.123]]) # 生成传递数据
+x = torch.rand(1000,2)
 model(x).shape # 前向传递-返回输出张量的形状
-model.plot() # 可视化
+model.plot(beta=10000, in_vars=[r'x', 'y'], out_vars=['f'], title = 'model_network')#可视化beta决定激活的尺度，即此数据输入时激活情况
+#model.plot() # 前向传递-返回输出张量的形状
 plt.show()
+# print(model(x))
 
-# 激活函数的表示：activation(x) = base_function(x) + spline_correction(x)，以silu函数为基础，利用B样条函数进行校正
+# 激活函数的表示：activation(x) = base_function(x) + spline_correction(x)，以silu函数
