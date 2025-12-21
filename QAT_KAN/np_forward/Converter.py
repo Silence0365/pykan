d3a22b9a827ac7ee3b290ccd1d0ecaaf0ff29f10
Silence0365 <1161378802@qq.com>
2025-12-21 23:16:25 +0800

    修改量化逻辑，匹配Qmn量化

--- QAT_KAN/np_forward/Converter.py
+++ QAT_KAN/np_forward/Converter.py
@@ -6,7 +6,7 @@ import numpy as np
 def QAT_Parameter_convert(model_path):
     # === model load ===
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = QATKAN(width=[3, 5, 1], grid=5, k=3, device=device,QAT_mode = True,num_bits= 16)
+    model = QATKAN(width=[3, 5, 1], grid=5, k=3, device=device,QAT_mode = True,M_num_bits= 7,N_num_bits =16)
     model.load_state_dict(torch.load(model_path, map_location=device),strict=True)
     i = 0
     num_layers = len(model.layers)
@@ -21,19 +21,25 @@ def QAT_Parameter_convert(model_path):
         mask        = layer.mask.detach().cpu().numpy()
         scale_base  = layer.scale_base.detach().cpu().numpy()
         spline_base = layer.scale_sp.detach().cpu().numpy()
+        M_num_bits   = layer.M_num_bits
+        N_num_bits  = layer.N_num_bits
 
-        coef_int, coef_proportion = quantizer(coef)
-        scale_base_int,   sb_proportion   = quantizer(scale_base)
-        spline_base_int,   sp_proportion   = quantizer(spline_base)
+        coef_int, coef_proportion         = quantizer(coef,M_num_bits ,N_num_bits)
+        scale_base_int,   sb_proportion   = quantizer(scale_base,M_num_bits ,N_num_bits)
+        spline_base_int,  sp_proportion   = quantizer(spline_base,M_num_bits ,N_num_bits)
+        grid_int,grid_proportion          = quantizer(grid,M_num_bits ,N_num_bits)
+        mask_int,mask_proportion          = quantizer(mask,M_num_bits ,0)#mask 没必要
 
-        model_parameters[f'layer{i}_grid']                     = grid
+        model_parameters[f'layer{i}_grid']                     = grid_int
         model_parameters[f'layer{i}_coef']                     = coef_int
         model_parameters[f'layer{i}_coef_proportion']          = coef_proportion
-        model_parameters[f'layer{i}_mask']                     = mask
+        model_parameters[f'layer{i}_mask']                     = mask_int
         model_parameters[f'layer{i}_scale_base']               = scale_base_int
         model_parameters[f'layer{i}_scale_base_proportion']    = sb_proportion
         model_parameters[f'layer{i}_spline_base']              = spline_base_int
-        model_parameters[f'layer{i}_spline_proportion']         = sp_proportion
+        model_parameters[f'layer{i}_spline_proportion']        = sp_proportion
+        model_parameters[f'layer{i}_QAT_M_num_bits']           = np.array(M_num_bits)
+        model_parameters[f'layer{i}_QAT_N_num_bits']           = np.array(N_num_bits)
 
     # 获取当前脚本所在目录
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -46,34 +52,22 @@ def QAT_Parameter_convert(model_path):
 
 
 
-def quantizer(x: np.ndarray, num_bits=16):
-    """
-    对称量化：float32 -> intN + scale
-    x: numpy array (float32)
-    returns: (x_int: int16/int8, scale: float32)
-    """
-    assert num_bits in [8, 16], "Only int8/int16 supported"
-    n = 2 ** (num_bits - 1) - 1  # 127 for int8, 32767 for int16
-    # 量化标度尺
-    x_abs_max = np.abs(x).max()
-    if x_abs_max == 0:
-        # 输入为0 --scale为1&x_int为0
-        scale = np.array(1.0, dtype=np.float32)
-        if (num_bits == 8):
-            x_int = np.zeros_like(x, dtype=np.int8)
-        else:
-            x_int = np.zeros_like(x, dtype=np.int16)
-    else:
-        scale = np.array(x_abs_max / n, dtype=np.float32)
-        # 限制范围
-        x_int = np.round(x / scale).clip( -n - 1, n)
-        if (num_bits == 8):
-            x_int = x_int.astype(np.int8)
-        else:
-            # 改变x_int的type(数据类型)
-            x_int = x_int.astype(np.int16)
-    return x_int, scale
-
+def quantizer(x: np.ndarray,M_num_bits=7,N_num_bits=8):
+    # Qm.n
+    # M_num_bits:整数位数
+    # N_num_bits:小数位数
+    total_bits = 1 + M_num_bits + N_num_bits
+    # print(f"M_bits{M_num_bits},N_bits{N_num_bits},total_bits{total_bits}")
+    quant_max = 2 ** (total_bits-1) - 1
+    quant_min = -2 ** (total_bits-1)
+    scale = 2**(-N_num_bits)
+    x_int = np.round(x / scale)
+    #取饱和，防止越界——超过上限取上限，低于下限取下限
+    if x_int.max() > quant_max or x_int.min() < quant_min:
+        x_int =np.clip(x_int,quant_min,quant_max)
+        print("warning:overflow")
+    x_int = x_int.astype(np.int64) 
+    return x_int,scale
 
 # check model layer
 # model.act_fun[i] # => KAN Layer (Spline)
@@ -83,6 +77,3 @@ def quantizer(x: np.ndarray, num_bits=16):
 # model.act_fun[i].coef
 # model.symbolic_fun[i].funs_name
 # model.symbolic_fun[i].mask
-
-#==================TEST==========================
-QAT_Parameter_convert('QAT_KAN/checkpoints/QAT_model.pth')
