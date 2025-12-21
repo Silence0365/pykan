d961dc5f8f80c2b1fb79a30eae125b6011a73484
Silence0365 <1161378802@qq.com>
2025-12-21 23:14:54 +0800

    适配定点数量化Qmn模型

--- QAT_KAN/np_forward/forward.py
+++ QAT_KAN/np_forward/forward.py
@@ -1,6 +1,6 @@
 from model_Parameter import Parameter_convert
 from KANLayer_forward import model_deduction
-from Converter import QAT_Parameter_convert
+from Converter import *
 from Descriptor_builder import simple_descriptor_builder
 from Desc_differential import desc_differentialer
 
@@ -9,15 +9,16 @@ import numpy as np
 coord_dir = '././MD/process_data/coord.npy'
 QAT_Parameter_convert('QAT_KAN/checkpoints/QAT_model.pth')
 x = simple_descriptor_builder(coord_dir)
-data = np.load('H2O_KAN/np_forward/model.npz')
-y,grad = model_deduction(x,data)
+x_QAT,scale = quantizer(x,M_num_bits=7,N_num_bits=16)
+data = np.load('QAT_KAN/np_forward/QAT_model.npz')
+y,grad = model_deduction(x_QAT,data)
 #print(f"force.shape{force.shape}")
 # print(f"final_energy{y}")
 # print(f"final_force{force}")
-
+print(f"{y[:3]* (2**-16)}")
 # === force detect ===
 dD_dR = desc_differentialer(coord_dir) # shape(batch, in_dim, parameter)
 
-# broadcast 
+# # broadcast 
 F = -  np.sum(grad[:, :, None] * dD_dR, axis =1)
-print(f"{F}")
+print(f"{F*(2**-32)}")# grad、dD_dR各有scale(2**-16)-->2**-32
